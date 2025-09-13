d06043935065af7adece7f976d534837d2ba86de
EmiRivero2003 <eriveronovo@byupathway.edu>
2025-09-13 14:48:11 -0300

    add week 02 temples project

---
+++ week02/temples/scripts-temples/temples.js
@@ -0,0 +1,15 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+const lastModDate = new Date(document.lastModified);
+
+const options = {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit"
+};
+
+const formatteDate = lastModDate.toLocaleDateString("en-US", options);
+
+document.getElementById("lastModified").textContent = `Last Modified: ${formatteDate}`;
