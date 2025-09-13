c42d83d0754a284e411f80f4f2a20775133bab6a
EmiRivero2003 <eriveronovo@byupathway.edu>
2025-09-13 17:18:42 -0300

    update

--- scripts-temples/temples.js
+++ scripts-temples/temples.js
@@ -1,7 +1,6 @@
 const yearEl = document.getElementById("currentyear");
 if (yearEl) yearEl.textContent = new Date().getFullYear();
 
-
 const lmEl = document.getElementById("lastModified");
 if (lmEl) {
   const lastModDate = new Date(document.lastModified);
@@ -13,8 +12,7 @@ if (lmEl) {
     minute: "2-digit",
     hour12: true
   };
-  const locale = navigator.language || "es-UY";
-  lmEl.textContent = `Last Modified: ${lastModDate.toLocaleString(locale, options)}`;
+  lmEl.textContent = `Last Modified: ${lastModDate.toLocaleString("en-US", options)}`;
 }
 
 const btn = document.getElementById("hamburger");
@@ -26,15 +24,4 @@ if (btn && nav) {
     btn.setAttribute("aria-expanded", String(isOpen));
     btn.textContent = isOpen ? "✕" : "☰";
   });
-
-
-  const resetForDesktop = () => {
-    if (window.matchMedia("(min-width: 768px)").matches) {
-      nav.classList.add("open");
-      btn.textContent = "☰";
-      btn.setAttribute("aria-expanded", "true");
-    }
-  };
-  resetForDesktop();
-  window.addEventListener("resize", resetForDesktop);
-}window.addEventListener("resize", resetForDesktop);
+}
