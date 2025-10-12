d1a403a8f7aae5e958cd18d2c0114aaf0ad8bbe3
EmiRivero2003 <eriveronovo@byupathway.edu>
2025-10-12 01:39:20 -0300

    FINISH

--- final-project/scripts-gamer-zone/peripherals.js
+++ final-project/scripts-gamer-zone/peripherals.js
@@ -149,3 +149,11 @@ document.addEventListener("DOMContentLoaded", () => {
   bindEvents();
 });
 
+// === HAMBURGER MENU FUNCTIONALITY ===
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
